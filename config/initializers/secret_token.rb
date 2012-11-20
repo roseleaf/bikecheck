944d87c457567fdf9891704120ae9cdb23ca3ce6
roseleaf <pteroline@gmail.com>
2012-11-20 10:40:50 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikecheck::Application.config.secret_token = '612313182afb1a788d3c41a125b174629670cc18808e48b59ea671ac2e8ad2a5a41e290070bf2386c571f56d8f85b23ef2b37aeb42dcddfaf7e622cf1e81c4c4'
